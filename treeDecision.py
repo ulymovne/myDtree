4dd88dec412123d1cad4bd4cbe8faf893253a858
ulymov <ulymovne@ya.ru>
2022-10-16 13:34:25 +0300

    обработка всех строк

--- treeDecision.py
+++ treeDecision.py
@@ -1,17 +1,19 @@
 class DecisionTree:
 
     @classmethod
-    def predict(cls, root, x):
-        cur_pos = root
-        while True:
-            i = cur_pos.indx
-            if x[i] <= cur_pos.value:
-                cur_pos = cur_pos.left
-            else:
-                cur_pos = cur_pos.right
-            if not cur_pos.target is None:
-                return cur_pos.target
-
+    def predict(cls, root, X):
+        # пока без numpy
+        y_predict = []
+        for x in X:
+            cur_pos = root
+            while cur_pos.target is None:
+                i = cur_pos.indx
+                if x[i] <= cur_pos.value:
+                    cur_pos = cur_pos.left
+                else:
+                    cur_pos = cur_pos.right
+            y_predict.append(cur_pos.target)
+        return y_predict
     @classmethod
     def add_obj(cls, obj, node=None, left=True):
         if not node is None:
@@ -20,6 +22,16 @@ class DecisionTree:
             else:
                 node.right =obj
         return obj
+
+    @staticmethod
+    def accuracy(y_true, y_pred):
+        try:
+            result = [y_true[i] == y_pred[i] for i in range(len(y_true))]
+            return round(sum(result) / len(result), 2)
+        except:
+            return "Error"
+
+
 class TreeObj:
 
     def __init__(self, indx, value=None, target=None):
@@ -67,10 +79,14 @@ DecisionTree.add_obj(TreeObj(-1, None, 2), v_121)
 DecisionTree.add_obj(TreeObj(-1, None, 1), v_121, False)
 
 # берем данные из файла iris2.csv
-x1 = [7.0, 3.2, 4.7, 1.4] # target = 1
-x2 = [5.8, 2.7, 5.1, 1.9] # target = 2
-x3 = [6.2, 3.4, 5.4, 2.3] # target = 2
-x4 = [6.7, 3.1, 4.7, 1.5] # target = 1
+X = []
+y = []
+with open('iris2.csv') as file:
+    file.readline()
+    for data in file.readlines():
+        str_ = data.strip().split(',')
+        X.append(list(map(float, str_[1:5])))
+        y.append(int(str_[5]))
+res = DecisionTree.predict(root, X)
 
-res = DecisionTree.predict(root, x2)
-print(res)
+print(DecisionTree.accuracy(y, res))
